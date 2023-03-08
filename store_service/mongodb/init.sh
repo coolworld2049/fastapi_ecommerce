94c12c2c8e3d3997f42cc1e6d540efa82292bcb0
coolworld <coolworld2049@gmail.com>
2023-03-08 04:09:55 +0300

    mongodb cluster, build.sh

---
+++ store_service/mongodb/init.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+set -e
+
+docker-compose exec configsvr01 sh -c "mongosh < /scripts/init-configserver.js"
+docker-compose exec shard01-a sh -c "mongosh < /scripts/init-shard01.js"
+docker-compose exec shard02-a sh -c "mongosh < /scripts/init-shard02.js"
+
+docker-compose exec router01 sh -c "mongosh < /scripts/init-router.js"
---
+++ store_service/mongodb/verify.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+set -e
+
+docker-compose exec router01 mongosh --port 27017 --eval 'sh.status()'
+
+docker exec -it shard-01-node-a bash -c "echo 'rs.status()' | mongosh --port 27017"
+docker exec -it shard-02-node-a bash -c "echo 'rs.status()' | mongosh --port 27017"
