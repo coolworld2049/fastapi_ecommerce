522003b28e988fc71094f8be30a7ff2f33e0876c
coolworld <coolworld2049@gmail.com>
2023-03-18 13:00:45 +0300

    prisma studio

--- store_service/prisma_studio/init.sh
+++ store_service/prisma_studio/init.sh
@@ -6,4 +6,4 @@ prisma generate --schema /prisma/schema.prisma
 
 prisma db push --schema /prisma/schema.prisma
 
-prisma studio --port "${PRISMA_STUDIO_PORT}" --browser none
+prisma studio --port "${PRISMA_STUDIO_PORT:-5555}" --browser none
