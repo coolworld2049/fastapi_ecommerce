e45ead19c3e25221d5191823812479af4d6f6882
coolworld <coolworld2049@gmail.com>
2023-03-18 23:18:50 +0300

    Deployment on VDS

--- store_service/scripts/mongodb/init_cluster.sh
+++ store_service/scripts/mongodb/init_cluster.sh
@@ -1,12 +1,11 @@
 #! /bin/bash -x
 
-set -e
+set +e
 
 docker-compose exec configsvr01 sh -c "mongosh < /scripts/init-configserver.js"
 docker-compose exec shard01-a sh -c "mongosh < /scripts/init-shard01.js"
 docker-compose exec shard02-a sh -c "mongosh < /scripts/init-shard02.js"
 docker-compose exec router01 sh -c "mongosh < /scripts/init-router.js"
-
 
 docker-compose exec router01 mongosh --port 27017 --eval 'sh.enableSharding("app")'
 docker-compose exec router01 mongosh --port 27017 --eval '
@@ -15,18 +14,16 @@ db.adminCommand( { shardCollection: "app.Order", key: { _id: 1 } } ),
 db.adminCommand( { shardCollection: "app.OrderProduct", key: { _id: 1 } } )
 '
 
-
 docker-compose exec router01 mongosh --port 27017 --eval 'sh.status()'
-docker exec -it shard-01-node-a bash -c "echo 'rs.status()' | mongosh --port 27017"
-docker exec -it shard-02-node-a bash -c "echo 'rs.status()' | mongosh --port 27017"
-
+docker exec shard-01-node-a bash -c "echo 'rs.status()' | mongosh --port 27017"
+docker exec shard-02-node-a bash -c "echo 'rs.status()' | mongosh --port 27017"
 
 #docker-compose exec router01 mongosh --port 27017 --eval '
 #use app,
 #db.stats(),
 #db.User.getShardDistribution()
 #'
-#docker exec -it mongo-config-01 bash -c "echo 'rs.status()' | mongosh --port 27017"
-#docker exec -it shard-01-node-a bash -c "echo 'rs.status()' | mongosh --port 27017"
-#docker exec -it shard-01-node-a bash -c "echo 'rs.printReplicationInfo()' | mongosh --port 27017"
-#docker exec -it shard-01-node-a bash -c "echo 'rs.printSecondaryReplicationInfo()' | mongosh --port 27017"
+#docker exec  mongo-config-01 bash -c "echo 'rs.status()' | mongosh --port 27017"
+#docker exec  shard-01-node-a bash -c "echo 'rs.status()' | mongosh --port 27017"
+#docker exec  shard-01-node-a bash -c "echo 'rs.printReplicationInfo()' | mongosh --port 27017"
+#docker exec  shard-01-node-a bash -c "echo 'rs.printSecondaryReplicationInfo()' | mongosh --port 27017"
