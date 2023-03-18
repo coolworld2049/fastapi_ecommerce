2119444e2239a21aaaf3dfac6dd42c18d07474fd
coolworld <coolworld2049@gmail.com>
2023-03-18 18:01:03 +0300

    deploy

--- build.prod.sh
+++ build.prod.sh
@@ -2,13 +2,17 @@
 
 set -e
 
+cp .env.example .env
+
 cd ./auth_service
+cp .env.example .env
 docker-compose up -d
 cd ..
 
 sleep 10
 
 cd ./store_service
+cp .env.example .env
 docker-compose up -d
 chmod +x ./scripts/mongodb/init_cluster.sh
 . ./scripts/mongodb/init_cluster.sh
@@ -17,6 +21,7 @@ cd ..
 sleep 10
 
 cd ./proxy
+cp .env.example .env
 chmod +x mkcert.sh
 . ./mkcert.sh
 docker-compose up -d
