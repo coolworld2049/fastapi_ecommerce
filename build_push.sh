1e511c4a528820fcd5ad6cdc60b8b507c138c3e0
coolworld <coolworld2049@gmail.com>
2023-03-19 14:29:08 +0300

    Deployment on VDS

--- build_push.sh
+++ build_push.sh
@@ -3,23 +3,23 @@
 set -e
 
 # shellcheck disable=SC2046
-source .env
+source .env;
 
-docker build -t "${AUTH_SERVICE_IMAGE}":latest ./auth_service
+docker build -t "${AUTH_SERVICE_IMAGE}" ./auth_service
 # shellcheck disable=SC2086
 echo "${DOCKER_PASSWORD}" | docker login -u ${DOCKER_USER} --password-stdin
 # shellcheck disable=SC2086
-docker push ${AUTH_SERVICE_IMAGE}:latest
+docker push ${AUTH_SERVICE_IMAGE}
 
 # shellcheck disable=SC2086
-docker build -t ${STORE_SERVICE_IMAGE}:latest ./store_service
+docker build -t ${STORE_SERVICE_IMAGE} ./store_service
 # shellcheck disable=SC2086
 echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USER} --password-stdin
-docker push "${STORE_SERVICE_IMAGE}":latest
+docker push "${STORE_SERVICE_IMAGE}"
 
 # shellcheck disable=SC2086
-docker build -t ${PROXY_IMAGE}:latest ./proxy
+docker build -t ${PROXY_IMAGE} ./proxy
 # shellcheck disable=SC2086
 echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USER} --password-stdin
 # shellcheck disable=SC2086
-docker push ${PROXY_IMAGE}:latest
+docker push ${PROXY_IMAGE}
