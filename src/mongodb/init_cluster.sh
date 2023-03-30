4f74cf7ef5a00b2b8b8aa80edbb0be81cfdbb2a1
coolworld <coolworld2049@gmail.com>
2023-03-31 01:34:40 +0300

    scripts

--- src/mongodb/init_cluster.sh
+++ src/mongodb/init_cluster.sh
@@ -2,10 +2,10 @@
 
 set +e
 
-docker-compose exec configsvr01 sh -c "mongosh < /scripts/init-configserver.js"
-docker-compose exec shard01-a sh -c "mongosh < /scripts/init-shard01.js"
-docker-compose exec shard02-a sh -c "mongosh < /scripts/init-shard02.js"
-docker-compose exec router01 sh -c "mongosh < /scripts/init-router.js"
+docker-compose exec configsvr01 bash -c "mongosh < /scripts/init-configserver.js"
+docker-compose exec shard01-a bash -c "mongosh < /scripts/init-shard01.js"
+docker-compose exec shard02-a bash -c "mongosh < /scripts/init-shard02.js"
+docker-compose exec router01 bash -c "mongosh < /scripts/init-router.js"
 
 docker-compose exec router01 mongosh --port 27017 --eval 'sh.enableSharding("app")'
 docker-compose exec router01 mongosh --port 27017 --eval '
