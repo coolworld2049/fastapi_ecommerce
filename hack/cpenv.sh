143d521746a5396ae0613f3f367a7b818d799430
coolworld <coolworld2049@gmail.com>
2023-04-03 14:05:54 +0300

    :bug: deploy.yml

--- hack/cpenv.sh
+++ hack/cpenv.sh
@@ -8,14 +8,14 @@ for dir in ../src/*; do
   env_f=.env."$(basename "$dir")"
   env_f_example="$env_f".example
   if [ -f ../src/"$env_f_example" ]; then
-    #cp ../src/"$env_f_example" ../src/"$env_f"
+    cp ../src/"$env_f_example" ../src/"$env_f"
     log "✅  created ../src/$env_f"
   else
     log "❌ ../src/$env_f_example File not exist"
   fi
 
   if [ -f "$dir"/.env.example ]; then
-    #cp "$dir"/.env.example "$dir"/.env
+    cp "$dir"/.env.example "$dir"/.env
     log "✅  created $dir/.env"
   else
     log "❌ $dir/.env.example File not exist"
